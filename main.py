ff743a2912a966d96ac60783981bd4c5c9b16397
Max Kendall <66796793+mpkendall@users.noreply.github.com>
2025-08-09 12:18:55 -0400

    test messaging

---
+++ main.py
@@ -0,0 +1,113 @@
+import badge
+
+class App(badge.BaseApp):
+    def __init__(self):
+        self.old_button_a = False
+        self.old_button_b = False
+        self.old_button_c = False
+        self.old_button_d = False
+        self.old_button_e = False
+        self.old_button_f = False
+        self.old_button_g = False
+        self.old_button_h = False
+        self.old_button_i = False
+        self.old_button_j = False
+        self.old_button_k = False
+        self.old_button_l = False
+
+    def on_open(self):
+        badge.display.fill(1) 
+        badge.display.text("William Daniel", 0, 0, 0)
+        badge.display.show()
+
+    def loop(self):
+        if badge.input.get_button(badge.input.Buttons.SW17):
+            self.old_button_a = True
+        else:
+            if self.old_button_a:
+                print("Button A pressed")
+                badge.radio.send_packet(0x5128, b'test')
+            self.old_button_a = False
+
+        if badge.input.get_button(badge.input.Buttons.SW10):
+            self.old_button_b = True
+        else:
+            if self.old_button_b:
+                print("Button B pressed")
+            self.old_button_b = False
+
+        if badge.input.get_button(badge.input.Buttons.SW18):
+            self.old_button_c = True
+        else:
+            if self.old_button_c:
+                print("Button C pressed")
+            self.old_button_c = False
+
+        if badge.input.get_button(badge.input.Buttons.SW9):
+            self.old_button_d = True
+        else:
+            if self.old_button_d:
+                print("Button D pressed")
+            self.old_button_d = False
+
+        if badge.input.get_button(badge.input.Buttons.SW15):
+            self.old_button_e = True
+        else:
+            if self.old_button_e:
+                print("Button E pressed")
+            self.old_button_e = False
+
+        if badge.input.get_button(badge.input.Buttons.SW8):
+            self.old_button_f = True
+        else:
+            if self.old_button_f:
+                print("Button F pressed")
+            self.old_button_f = False
+
+        if badge.input.get_button(badge.input.Buttons.SW16):
+            self.old_button_g = True
+        else:
+            if self.old_button_g:
+                print("Button G pressed")
+            self.old_button_g = False
+
+        if badge.input.get_button(badge.input.Buttons.SW7):
+            self.old_button_h = True
+        else:
+            if self.old_button_h:
+                print("Button H pressed")
+            self.old_button_h = False
+
+        if badge.input.get_button(badge.input.Buttons.SW13):
+            self.old_button_i = True
+        else:
+            if self.old_button_i:
+                print("Button I pressed")
+            self.old_button_i = False
+
+        if badge.input.get_button(badge.input.Buttons.SW6):
+            self.old_button_j = True
+        else:
+            if self.old_button_j:
+                print("Button J pressed")
+            self.old_button_j = False
+
+        if badge.input.get_button(badge.input.Buttons.SW14):
+            self.old_button_k = True
+        else:
+            if self.old_button_k:
+                print("Button K pressed")
+            self.old_button_k = False
+
+        if badge.input.get_button(badge.input.Buttons.SW5):
+            self.old_button_l = True
+        else:
+            if self.old_button_l:
+                print("Button L pressed")
+            self.old_button_l = False
+
+    def on_packet(self, packet: badge.radio.Packet, in_foreground: bool):
+        badge.display.fill(1)
+        badge.display.text(f"From: {packet.source}", 0, 0, 0)
+        badge.display.show()
+        print(f"Received packet: {packet}")
